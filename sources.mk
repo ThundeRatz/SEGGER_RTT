7c1a726383a8acaaaf368fcf02b1fbe7ce617db3
LucasHaug <lucas.haug@hotmail.com>
2020-12-17 15:39:29 -0300

    :fire: remove unused flag

--- sources.mk
+++ sources.mk
@@ -7,9 +7,6 @@ LIB_SOURCES += $(shell find $(THIS_PATH) -name "*.c")
 
 undefine THIS_PATH
 
-# Specifies if this lib is being used
-USE_RTT_LIB := 1
-
 # Start Debug Session
 rtt:
 	$(AT)JLinkGDBServer -device $(DEVICE) -nohalt -if SWD -speed 4000 -port 2331 -vd -singlerun -timeout 0 -nogui > /dev/null &
